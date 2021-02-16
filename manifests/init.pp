d3291ca506c708170952e515e64ac241d62b5c92
James Lane <61185185+jameslanepuppet@users.noreply.github.com>
2021-02-16 16:42:32 +0000

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,56 +17,9 @@
 #   }
 class vra_puppet_plugin_prep (
   String  $vro_plugin_user,
-  String  $vro_password,
-  String  $vro_password_hash,
-  Boolean $manage_autosign,
   Boolean $manage_localuser,
   Boolean $manage_sshd,
-  String  $autosign_secret,
-  String  $vro_email,
-  String  $vro_display_name,
 ) {
-
-  $vro_role_name = 'VRO Plugin User'
-  $permissions   = [
-    { 'object_type' => 'cert_requests',
-      'action'      => 'accept_reject',
-      'instance'    => '*',
-    },
-    { 'object_type' => 'tasks',
-      'action'      => 'run',
-      'instance'    => '*',
-    },
-    { 'object_type' => 'nodes',
-      'action'      => 'view_data',
-      'instance'    => '*',
-    },
-    { 'object_type' => 'orchestrator',
-      'action'      => 'view',
-      'instance'    => '*',
-    },
-    { 'object_type' => 'puppet_agent',
-      'action'      => 'run',
-      'instance'    => '*',
-    },
-  ]
-
-  rbac_role { $vro_role_name:
-    ensure      => present,
-    name        => $vro_role_name,
-    description => $vro_role_name,
-    permissions => $permissions,
-  }
-
-  rbac_user { $vro_plugin_user:
-    ensure       => 'present',
-    name         => $vro_plugin_user,
-    display_name => $vro_display_name,
-    password     => $vro_password,
-    roles        => [ $vro_role_name ],
-    email        => $vro_email,
-    require      => Rbac_role[$vro_role_name],
-  }
 
   if $manage_localuser {
     user { $vro_plugin_user:
@@ -105,26 +58,6 @@ class vra_puppet_plugin_prep (
   package { 'puppet-strings':
     ensure   => present,
     provider => puppet_gem,
-  }
-
-  if $manage_autosign {
-    file { '/etc/puppetlabs/puppet/autosign.rb' :
-      ensure  => file,
-      owner   => 'pe-puppet',
-      group   => 'pe-puppet',
-      mode    => '0700',
-      content => epp('vra_puppet_plugin_prep/autosign.rb.epp', { 'autosign_secret' => $autosign_secret }),
-      notify  => Service['pe-puppetserver'],
-    }
-
-    ini_setting { 'autosign script setting':
-      ensure  => present,
-      path    => '/etc/puppetlabs/puppet/puppet.conf',
-      section => 'master',
-      setting => 'autosign',
-      value   => '/etc/puppetlabs/puppet/autosign.rb',
-      notify  => Service['pe-puppetserver'],
-    }
   }
 }
 
